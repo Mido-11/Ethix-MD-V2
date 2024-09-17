533b6afbbab5c290b7d1308555fc049e5df5d8b9
ᴍɪᴅᴏ <96103438+Mido-11@users.noreply.github.com>
2024-09-17 17:20:06 +0530

    lyrics.js

--- src/plugin/lyrics.js
+++ src/plugin/lyrics.js
@@ -15,7 +15,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
     try {
       await m.React('🕘');
-      await m.reply('A moment, *Ethix-MD* is generating your lyrics request...');
+      await m.reply('A moment, *MIDOBOT* is generating your lyrics request...');
 
       if (!text.includes('|')) {
         return m.reply('Please provide the song name and artist name separated by a "|", for example: Spectre|Alan Walker.');
